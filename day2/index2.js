65aec78bc2aabd1ef2982dda53a659498a45d931
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-05-28 15:59:35 +0530

    Create index2.js

---
+++ day2/index2.js
@@ -0,0 +1,15 @@
+//type conversion
+let age=window.prompt("enter your age");
+age=Number(age);
+age+=1;
+console.log(age);
+
+let x=12;
+let y='amm';
+let z;
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
