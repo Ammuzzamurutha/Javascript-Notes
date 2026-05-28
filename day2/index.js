c0788485dc32f7ffef8f73f3407a4fbc71bef7f0
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-05-28 15:13:28 +0530

    Update index.js

--- day2/index.js
+++ day2/index.js
@@ -1,1 +1,27 @@
+let student=30;
+//operators
+//arithmetic operators
+student+=1;
+console.log(student);
+student-=1;
+console.log(student);
+student*=2;
+console.log(student);
+student/=2;
+console.log(student);
+student%=2;
+console.log(student);
+student**=2;
+console.log(student);
 
+student++;
+student--;
+console.log(student);
+
+//expressions
+//parenthesis
+//exponentiation
+//multiplication ,division ,modulus
+//addition and subtraction
+let res=2**3+(6/2)-4+7;//2**3=8,6/2=3,8+3-4+7=14
+console.log(res);
